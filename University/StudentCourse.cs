580ca5c45cf131c0174413b0c71bcffe606e3510
Aliaksandr.Kuzmik <aliaksandr.kuzmik@itechart-group.com>
2020-09-04 11:47:13 +0300

    Added project files

---
+++ University/StudentCourse.cs
@@ -0,0 +1,9 @@
+﻿namespace University
+{
+    public class StudentCourse
+    {
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+        public int Grade { get; set; }
+    }
+}
